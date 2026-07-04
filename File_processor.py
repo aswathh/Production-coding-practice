e982932b42b3ceb28ab8176032f6c136604be281
aswathh <aswathkumar2001@gmail.com>
2026-07-04 15:32:09 +0530

    file_processor.py added

--- File_processor.py
+++ File_processor.py
@@ -1,31 +1,32 @@
 import os
+from typing import List
 import logging
 from dotenv import load_dotenv
-from typing import List
 
 load_dotenv()
-logging.basicConfig(level=logging.INFO,format='%(asctime)s-%(levelname)s-%(message)s')
+
+logging.basicConfig(level=logging.INFO,format='%(asctime)s-%(filename)s-%(levelname)s-%(message)s')
 logger = logging.getLogger(__name__)
 
-def read_numbers(file_path:str)->List[int]:
-    numbers:List[int]=[]
+def get_numbers(file_path:str)->List[int]:
+    numbers:List[int] = []
     try:
         with open(file_path,"r") as f:
             for line in f:
-                line = line.strip() 
+                line = line.strip()
                 if line:
                     numbers.append(int(line))
-        logger.info(f"Successfully read {len(numbers)} numbers from the {file_path}")
+        logging.info(f"Successfully read {len(numbers)} numbers from {filepath}")
     except FileNotFoundError:
-        logger.error(f"file not found{file_path}",exc_info = True)
+        logging.error(f"File not found in the path:{file_path}")
     except ValueError:
-        logger.error(f"Invalid number in the given file:{file_path}",exc_info=True)
-    
+        logging.error(f"Invalid number in the file {file_path}")
     return numbers
-input_path:str =os.getenv("FILE_PATH","Fileprocessor.txt")
-numbers = read_numbers(input_path)
+
+numbers = os.getenv("FILE_PATH","Fileprocessor.txt")
+
 if numbers:
     total = sum(numbers)
-    logger.info(f"Total sum:",{total})
+    logger.info(f"sum of numbers:{total}")
 else:
-    logger.warning("No numbers found in the process")
+    logger.warning("No numbers found to process")
